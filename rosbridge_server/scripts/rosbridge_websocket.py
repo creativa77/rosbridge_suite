c72abe204bee12efb19a1e1cee5651a47aae9626
Ivan Arcuschin <iarcuschin@gmail.com>
2015-03-09 17:41:07 -0300

    Added user auth param and call to new roauth service

--- rosbridge_server/scripts/rosbridge_websocket.py
+++ rosbridge_server/scripts/rosbridge_websocket.py
@@ -55,7 +55,7 @@ authenticate = False
 class RosbridgeWebSocket(WebSocketHandler):
 
     def open(self):
-        global client_id_seed, clients_connected, authenticate
+        global client_id_seed, clients_connected, authenticate, user_auth
         try:
             self.protocol = RosbridgeProtocol(client_id_seed)
             self.protocol.outgoing = self.send_message
@@ -69,7 +69,7 @@ class RosbridgeWebSocket(WebSocketHandler):
             rospy.loginfo("Awaiting proper authentication...")
 
     def on_message(self, message):
-        global authenticate
+        global authenticate, user_auth
         # check if we need to authenticate
         if authenticate and not self.authenticated:
             try:
@@ -80,6 +80,24 @@ class RosbridgeWebSocket(WebSocketHandler):
                     resp = auth_srv(msg['mac'], msg['client'], msg['dest'], 
                                                   msg['rand'], rospy.Time(msg['t']), msg['level'], 
                                                   rospy.Time(msg['end']))
+                    self.authenticated = resp.authenticated
+                    if self.authenticated:
+                        rospy.loginfo("Client %d has authenticated.", self.protocol.client_id)
+                        return
+                # if we are here, no valid authentication was given
+                rospy.logwarn("Client %d did not authenticate. Closing connection.", 
+                              self.protocol.client_id)
+                self.close()
+            except:
+                # proper error will be handled in the protocol class
+                self.protocol.incoming(message)
+        elif user_auth and not self.authenticated:
+            try:
+                msg = json.loads(message)
+                if msg['op'] == 'auth':
+                    # check the authorization information
+                    auth_srv = rospy.ServiceProxy('authenticate_user', UserAuthentication)
+                    resp = auth_srv(msg['user'], msg['pass'])
                     self.authenticated = resp.authenticated
                     if self.authenticated:
                         rospy.loginfo("Client %d has authenticated.", self.protocol.client_id)
@@ -116,6 +134,7 @@ if __name__ == "__main__":
     keyfile = rospy.get_param('~keyfile', None)
     # if authentication should be used
     authenticate = rospy.get_param('~authenticate', False)
+    user_auth = rospy.get_param('~user_auth', False)
     port = rospy.get_param('~port', 9090)
     address = rospy.get_param('~address', "")
 
